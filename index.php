a253b8d8c5e071f54eea212623e224bf3fb81a5b
Renegado-ops <Erickssondiaz789@gmail.com>
2022-02-22 21:55:58 -0400

    linda

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <title>Lord´s</title>
+  </head>
+  <body>
+  <div class="col-lg-8 mx-auto p-3 py-md-5">
+  <header class="d-flex align-items-center pb-3 mb-5 border-bottom">
+    <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+      icono
+      <span class="fs-4">Lord´s</span>
+    </a>
+  </header>
+
+  <main>
+    <h1><i class="bi bi-wrench-adjustable"></i>Bienvenidos a Lord´s</h1>
+    <p class="fs-5 col-md-8">Las cosas son mejores como miembro de Lord´s. Obtenga acceso primero y exclusivo a los estilos e innovaciones más nuevos, envío gratis, recompensas de cumpleaños y más.</p>
+
+    <div class="mb-5">
+      <a href="/docs/5.1/examples/" class="btn btn-primary btn-lg px-4">Download examples</a>
+    </div>
+
+    <hr class="col-3 col-md-2 mb-5">
+
+    <div class="row g-5">
+      <div class="col-md-6">
+        <h2>Starter projects</h2>
+        <p>Ready to beyond the starter template? Check out these open source projects that you can quickly duplicate to a new GitHub repository.</p>
+        <ul class="icon-list">
+          <li><a href="https://github.com/twbs/bootstrap-npm-starter" rel="noopener" target="_blank">Bootstrap npm starter</a></li>
+          <li class="text-muted">Bootstrap Parcel starter (coming soon!)</li>
+        </ul>
+      </div>
+
+      <div class="col-md-6">
+        <h2>Guides</h2>
+        <p>Read more detailed instructions and documentation on using or contributing to Bootstrap.</p>
+        <ul class="icon-list">
+          <li><a href="/docs/5.1/getting-started/introduction/">Bootstrap quick start guide</a></li>
+          <li><a href="/docs/5.1/getting-started/webpack/">Bootstrap Webpack guide</a></li>
+          <li><a href="/docs/5.1/getting-started/parcel/">Bootstrap Parcel guide</a></li>
+          <li><a href="/docs/5.1/getting-started/contribute/">Contributing to Bootstrap</a></li>
+        </ul>
+      </div>
+    </div>
+  </main>
+  <footer class="pt-5 my-5 text-muted border-top">
+    Created by the Bootstrap erick &middot; &copy; 2022
+  </footer>
+</div>
+
+
+    <script src="/docs/5.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
