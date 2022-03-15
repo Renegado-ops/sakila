769ba126054a7447527952c9683a226ef42cb9d2
Renegado-ops <61117385+Renegado-ops@users.noreply.github.com>
2022-03-15 12:14:31 -0400

    vista_categoria y categoria

--- categorias.php
+++ categorias.php
@@ -4,7 +4,7 @@ require_once "recursos/conexion.php";
 require_once "recursos/funciones.php";
 $pagina = "Categorias";
 
-$error ="";
+$error = "";
 
 
 try {
@@ -15,30 +15,69 @@ try {
     echo "</pre>";
 
     //verificar si le da click al boton
-    if (isset ($_POST['boton-guardar'])){
+    if (isset($_POST['boton-guardar'])) {
+
+        //variables
+        $name = $_POST["name"];
+        //validaciones
+        if (empty($name)) {
+            throw new Exception("El nombre no puede estar vacio ");
+        }
+        //guardar
+
+        $query = "INSERT INTO category (name) VALUE ('$name')";
+
+        //actualizar
+        $id = $_POST['id'] ?? '';
+
+        if (!empty($id)) {
+            $query = "UPDATE category SET name = '$name' WHERE category_id = '$id' ";
+        }
+
+        $resultado = $conexion->query($query) or die("Error en query");
+        if ($resultado) {
+            $_SESSION['mensaje'] = "Datos insertados correctamente";
+        } else {
+            throw new Exception("No se puede insertar los datos");
+        }
+        //refrezcar
+
+        refrezcar('categorias.php');
+    }
+
+    //buscar info para editar
+    if (isset($_GET['editar'])) {
+        $category_id = $_GET['editar'];
+        $query = "SELECT * FROM category WHERE category_id = '$category_id' ";
+        $resultado = mysqli_query($conexion, $query);
+
+        if ($resultado) {
+            while ($fila = mysqli_fetch_object($resultado)) {
+                $category_id = $fila->category_id;
+                $name = $fila->name;
+                print_r($fila);
+            }
+        }
+    }
+
+    if (isset($_GET['eliminar'])) {
+        $category_id = $_GET['eliminar'];
         
-    //variables
-    $name = $_POST["name"];
-    //validaciones
-    if (empty($name)){
-        throw new Exception("El nombre no puede estar vacio "); 
-    }
-    //guardar
+        $query = "DELECT FROM category WHERE category_id = '$category_id' ";
+        $resultado = $conexion->query($query) or die ("Error en query");
 
-    $query = "INSERT INTO category (name) VALUE ('$name')";
+        if ($resultado){
+            $script_alerta = alerta("Eliminar", "Datos eliminados", "success");
+        }
+                else {
+                    $script_alerta = alerta("Error", "No se pudo eliminar", "error");
+                    throw new Exception("No se pudo eliminar los datos");
+                }
+            }
+        
     
-    $resultado = $conexion-> query($query) or die ("Error en query");
-    if ($resultado){
-        $_SESSION['mensaje'] = "Datos insertados correctamente";
-    }else{
-        throw new Exception("No se puede insertar los datos");
-    }
-    //refrezcar
-
-    refrezcar('categorias.php');
-}
-} catch(Throwable $ex){
-    $error = $ex ->getMessage();
+} catch (Throwable $ex) {
+    $error = $ex->getMessage();
 }
 require_once "vistas/vista_categorias.php";
 #no debe haber codigo despues de esta linea
